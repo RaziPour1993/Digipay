dd9e474d3dfe8acd9234aa0d31079548aacee8b6
Mohammad Razipour <razipour1993@gmail.com>
2019-12-24 13:19:34 +0330

    Init project and delete additional files

---
+++ Digipay/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Digipay
+//
+//  Created by Mohammad Razipour on 12/24/19.
+//  Copyright © 2019 Mohammad Razipour. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+    
+}
+
